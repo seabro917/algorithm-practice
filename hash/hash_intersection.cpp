87710392d75fea1dd9469efe1c0c566fd6539eaa
seabro97917 <hgzwmcf@gmail.com>
2023-05-30 19:27:35 +0100

    hash set intersection

--- hash/hash_intersection.cpp
+++ hash/hash_intersection.cpp
@@ -41,3 +41,19 @@ public:
         return result;
     }   
 };
+
+// 直接从两个输入的vector构造set
+class Solution_ {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> result_set; // 存放结果，之所以用set是为了给结果集去重
+        unordered_set<int> nums_set(nums1.begin(), nums1.end());
+        for (int num : nums2) {
+            // 发现nums2的元素 在nums_set里又出现过
+            if (nums_set.find(num) != nums_set.end()) {
+                result_set.insert(num);
+            }
+        }
+        return vector<int>(result_set.begin(), result_set.end());
+    }
+};
