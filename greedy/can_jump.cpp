179ade3120344cd733c99edb2b24b672927d80aa
seabro97917 <hgzwmcf@gmail.com>
2023-07-10 18:34:01 +0100

    second solutions

--- greedy/can_jump.cpp
+++ greedy/can_jump.cpp
@@ -11,6 +11,7 @@ public:
             return true;
         }
         for (int i = 0; i <= coverage; i++){
+            // cout << coverage << endl;
             coverage = max(i + nums[i], coverage);
             if(coverage >= nums.size() - 1){
                 return true;
@@ -19,3 +20,20 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > k) {
+               cout << i << endl; 
+               cout << k << endl; 
+               return false;
+            }
+            k = max(k, i + nums[i]);
+        }
+        return true;
+    }
+};
+
