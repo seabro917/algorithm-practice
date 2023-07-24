4ea92b77b98158d6544bbd91c61df026bb403825
Yuchen Mu <hgzwmcf@gmail.com>
2023-07-24 21:05:57 +0100

    binary tree traversal

---
+++ binary_tree/traversal_postorder.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> res;
+        if (root == NULL) {
+            return res;
+        }
+        // 先写一个中右左的遍历 --> 即在中左右的前序遍历的代码中调换左右的push
+        st.push(root);
+        while (!st.empty()) {
+            TreeNode* temp = st.top();
+            st.pop();
+            res.push_back(temp->val);
+            if (temp->left != NULL) {
+                st.push(temp->left);
+            }
+            if (temp->right != NULL) {
+                st.push(temp->right);
+            }
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+class Solution {
+private:
+    void traversal(TreeNode* node, vector<int>& res) {
+        if (node == NULL) {
+            return;
+        }
+        traversal(node->left, res);
+        traversal(node->right, res);
+        res.push_back(node->val);
+    }
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        traversal(root, res);
+        return res;
+    }
+};
---
+++ binary_tree/traversal_preorder.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> res;
+        if (root == NULL) {
+            return res;
+        }
+        st.push(root);
+        while (!st.empty()) {
+            TreeNode* temp = st.top();
+            st.pop();
+            res.push_back(temp->val);
+            if (temp->right != NULL) {
+                st.push(temp->right);
+            }
+            if (temp->left != NULL) {
+                st.push(temp->left);
+            }      
+        }
+        return res;
+    }
+};
+
+
+class Solution {
+private:
+    void traversal(TreeNode* node, vector<int>& res) {
+        if (node == NULL) {
+            return;
+        }
+        res.push_back(node->val);
+        traversal(node->left, res);
+        traversal(node->right, res);
+    }
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        traversal(root, res);
+        return res;
+    }
+};
