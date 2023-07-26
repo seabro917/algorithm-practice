923577451dcc2c5574e13705a101fc151eae89d6
Yuchen Mu <hgzwmcf@gmail.com>
2023-07-26 14:41:53 +0100

    level order N children

---
+++ binary_tree/levelOrder_Nchildren.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        queue<Node*> que;
+        vector<vector<int>> res;
+        if (root != nullptr) {
+            que.push(root);
+        }
+        while (!que.empty()) {
+            int size = que.size();
+            vector<int> temp;
+            for (int i = 0; i < size; i++) {
+                Node* cur = que.front();
+                que.pop();
+                temp.push_back(cur->val);
+                for (int j = 0; j < cur->children.size(); j++) {
+                    que.push(cur->children[j]);
+                }
+            }
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
