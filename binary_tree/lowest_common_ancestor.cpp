0470eb189549ff2476d7df441ee14d5c0d7ce20d
Yuchen Mu <hgzwmcf@gmail.com>
2023-08-02 21:29:54 +0100

    lowest common ancestor

---
+++ binary_tree/lowest_common_ancestor.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+//  leetcode 236
+// https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/solutions/240096/236-er-cha-shu-de-zui-jin-gong-gong-zu-xian-hou-xu/
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root) {
+            return nullptr;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if (!left && !right) {
+            return nullptr;
+        }
+        if (!left && right) {
+            return right;
+        }
+        if (left && !right) {
+            return left;
+        }
+        return root;  // left和right都不为空的情况,说明p和q分别位于root左右两侧,root即为要返回的最近公共祖先.
+    }
+};
