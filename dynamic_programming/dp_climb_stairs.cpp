a2602628553199c075ce273664c436bcbba025bc
Yuchen Mu <hgzwmcf@gmail.com>
2023-08-17 18:13:05 +0100

    add solution

--- dynamic_programming/dp_climb_stairs.cpp
+++ dynamic_programming/dp_climb_stairs.cpp
@@ -16,6 +16,27 @@ public:
 };
 
 
+// 完全背包解法
+// 实际就是求装满容量为n的方法的排列
+// 物品为每次可以爬的楼梯数目,这里就是2,可以设置为更多,比如一次最多可以爬3层或者4层...
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        int m = 2;
+        for (int j = 1; j <= n; j++) {
+            for (int i = 0; i <= m; i++) {
+                if (j >= i) {
+                    dp[j] += dp[j - i];
+                }
+            }
+        }
+        return dp[n];
+    }
+};
+
+
 
 // class Solution {
 // public:
