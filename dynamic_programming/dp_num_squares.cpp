45a84b6fb9610eddd85c9e3c75fa5b5aa452a8af
Yuchen Mu <hgzwmcf@gmail.com>
2023-08-18 22:27:54 +0100

    dp number of squares

---
+++ dynamic_programming/dp_num_squares.cpp
@@ -0,0 +1,16 @@
+// leetcode 279
+
+// 完全背包，求最少多少物品能装满容量为n的背包
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n + 1, INT32_MAX);
+        dp[0] = 0;
+        for (int i = 1; i * i <= n; i++) {
+            for (int j = i * i; j <= n; j++) {
+                dp[j] = min(dp[j], dp[j - i * i] + 1);
+            }
+        }
+        return dp[n];
+    }
+};
