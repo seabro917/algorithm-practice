2c46cf6032294d6d3ea516c6349349d1729813d7
Yuchen Mu <hgzwmcf@gmail.com>
2023-10-28 18:43:30 +0100

    array h index

---
+++ array/array_hIndex.cpp
@@ -0,0 +1,26 @@
+// leetcode 274
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        // 从有可能h的最大值开始一次次递减-->有可能的h的最大值就是数组里面的最大值
+        int max_val = *max_element(citations.begin(), citations.end());
+        int res = 0;
+        for (int i = max_val; i > 0; i--) {
+            int num_count = 0;
+            // 当前有多少篇文章满足被cite的次数大于等于当前这个h
+            for (int j = 0; j < citations.size(); j++) {
+                if (citations[j] >= max_val) {
+                    num_count++;
+                }
+            }
+            // 当前h满足条件，即有大于等于h篇文章被cite的次数大于等于h次
+            if (num_count >= max_val) {
+                res = max_val;
+                break;
+            }
+            // 当前的值不满足条件，放宽条件，递减h
+            max_val--;
+        }
+        return res;
+    }
+};
