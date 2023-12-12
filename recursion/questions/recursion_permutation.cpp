b540bf3ac20921124214efe64a67dda000ca6b31
Yuchen Mu <hgzwmcf@gmail.com>
2023-12-12 18:37:50 +0000

    recursion permutation solution update

--- recursion/questions/recursion_permutation.cpp
+++ recursion/questions/recursion_permutation.cpp
@@ -3,38 +3,30 @@
 using namespace std;
 
 // leetcode 46
+// leetcode LCR 083
+// b站那个教程的swap写法
 class Solution {
-  public:
-    vector<vector<int> > permute(vector<int>& num) {
-        vector<vector<int>> result;
-        // 第一次是从初始位置开始。
-        allpermute(num, 0, result);
-        return result;
-    }
-
-
-    void allpermute(vector<int>& num, int position, vector<vector<int>>& result) {
-        // 从数组的position位置开始全排列。
-        if (position == num.size() - 1) {
-            result.push_back(num);
+private: 
+    void backtracking(vector<int>& nums, int startIndex, vector<vector<int>>& res) {
+        if (startIndex == nums.size() - 1) {
+            res.push_back(nums);
             return;
         }
-        for (int i = position ; i < num.size() ; i++) {
-            swap(num[position], num[i]);
-            allpermute(num, position + 1, result);
-            // 恢复初始状态 --> 还原现场。
-            swap(num[position], num[i]);
+        // i不能从startIndex + 1开始,这样会少了以nums[startIndex]为开头的情况
+        // 虽然看上去i = startIndex时候swap(nums[startIndex], nums[i])这行没干事儿
+        // 但是实际上保证了nums[startIndex]为开头的情况能被顺利计算
+        for (int i = startIndex; i < nums.size(); i++) {
+            swap(nums[startIndex], nums[i]);
+            backtracking(nums, startIndex + 1, res);
+            swap(nums[startIndex], nums[i]);
         }
     }
-
-
-    void swap(int& a, int& b) {
-        int temp = 0;
-        temp = b;
-        b = a;
-        a = temp;
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        backtracking(nums, 0, res);
+        return res;
     }
 };
-
 
 
