b714fd20d1424a4a1f257980e5abb11557255c13
Yuchen Mu <hgzwmcf@gmail.com>
2024-01-14 17:35:19 +0000

    linked list remove duplicates

---
+++ linked_list/questions/linked_list_remove_duplicates.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// leetcode 82
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* cur = dummy;
+        while (cur->next && cur->next->next) {
+            if (cur->next->val == cur->next->next->val) {
+                // 这一步内存别省了
+                int val_temp = cur->next->val;
+                while (cur->next && cur->next->val == val_temp) {
+                    cur->next = cur->next->next;
+                }
+            }
+            else {
+                cur = cur->next;
+            }
+        }
+        return dummy->next;
+    }
+};
